e9b8a35a7689bca11608e266b27606dee9274f33
Maysha-Khanom-Moon <maysha412@gmail.com>
2026-08-30 01:15:24 +0600

    Add calculator error handling

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from utils import add, subtract, multiply
+from utils import add, subtract, multiply, divide
 
 name = "Maysha Khanom Moon"
 
@@ -9,3 +9,8 @@ print("Today's date:", date.today())
 print("Addition:", add(10, 5))
 print("Subtraction:", subtract(10, 5))
 print("Multiplication:", multiply(10, 5))
+
+try:
+    print("Division:", divide(10, 2))
+except ValueError as error:
+    print("Error:", error)
